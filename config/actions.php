23438a2e8145c7d772171ddbb735f08790e5aee3
MrAntoine <antoinevdb15@gmail.com>
2019-01-19 14:33:53 +0100

    MAJ fichiers

---
+++ config/actions.php
@@ -0,0 +1,11 @@
+<?php
+
+$listeDesActions = array(
+    "soundFilter" => "traitement/sound/filter.php",
+    "mentionslegales" => "vues/mentions_legales.php",
+    "signalements" => "vues/Signalement/Signalements_List.php",
+    "son" => "vues/sound/Sound_Page.php",
+    "like" => "vues/likes.php",
+    "profil" => "vues/user/profil.php",
+    "register" => "traitement/register.php"
+);
