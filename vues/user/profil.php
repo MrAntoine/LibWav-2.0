c7a8b8cb892c34adafdc55fa5c05d8ba3ace81fd
MrAntoine <antoinevdb15@gmail.com>
2019-01-20 14:13:41 +0100

    Ajout message needConnect

--- vues/user/profil.php
+++ vues/user/profil.php
@@ -9,6 +9,14 @@
 
 $autorisation = false;
 
+if (!isset($_GET["id"]) || ($_GET["id"]) == ($_SESSION["id"])) {
+    $id = $_SESSION["id"];
+} else {
+    $id = $_GET["id"];
+}
+include('vues/user/infos_user_avatar.php');
+
+
 if (isset($_SESSION["id"])) {
     $role = getUserInfo($_SESSION["id"])['statut'];
     if ($role >= roleUser("user")) {
@@ -18,30 +26,21 @@ if (isset($_SESSION["id"])) {
 
 if ($autorisation === true) {
 
-    // Affichage des avatars utilisateur //
-    if (!isset($_GET["id"]) || ($_GET["id"]) == ($_SESSION["id"])) {
-        $id = $_SESSION["id"];
-    } else {
-        $id = $_GET["id"];
-    }
-    include('vues/user/infos_user_avatar.php');
-
     echo " <p> Sexe: " . $affiche_user["sexe"] . "</p>";
     echo " <p> Anniversaire: " . $affiche_user["anniversaire"] . "</p>";
     echo " <p> Compte créé le: " . $affiche_user["created_at"] . "</p>";
-
-
+    echo " <p> nombre de téléchargements: " . $affiche_user["nb_telechargements"] . "</p>";
 
     if ($role >= roleUser("modo")) {
-
         $total = checkSignalementsUser($id);
             echo "<br/>"."Signalé : ".$total." fois";
-
     }
 
 
 
 
 
+} else {
+    echo needConnect();
 }
 ?>
