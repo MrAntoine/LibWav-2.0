9d7889fee32f4828f73f0cb396ed616d108c3bb7
MrAntoine <antoinevdb15@gmail.com>
2019-01-20 13:28:28 +0100

    Ajout page 404

---
+++ vues/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 20/01/2019
+ * Time: 13:27
+ */
+
+
+
+echo "<h1>Erreur 404</h1><p>La page demandé n'est pas disponible ou n'existe pas.</p>";
+
+
+
+?>
