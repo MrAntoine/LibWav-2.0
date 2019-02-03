2a523fdef29f918049f40402a29a83e5fa190dfa
MrAntoine <antoinevdb15@gmail.com>
2019-02-03 13:05:51 +0100

    Changement couleur like onclick

--- vues/likes.php
+++ vues/likes.php
@@ -6,8 +6,6 @@
  * Time: 18:58
  */
 
-
-
 $autorisation = false;
 
 if (isset($_SESSION["id"])) {
@@ -23,16 +21,18 @@ if ($autorisation === true) {
     $resultLike = checkLikes($_SESSION['id'],$_POST['idPost']);
 
     if ($resultLike == false) {
-        //style css
+        $data = "faux";
         addLikes($_SESSION['id'],$_POST['idPost']);
         header('Location: ' . $_SERVER['HTTP_REFERER'] );
+
     } else {
-        // style css
+        $data = "vrai";
         deleteLikes($_SESSION['id'],$_POST['idPost']);
         header('Location: ' . $_SERVER['HTTP_REFERER'] );
+
     }
 
-
-
+    echo $data;
 }
+
 ?>
