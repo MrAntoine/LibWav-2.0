ea8b5a75dcde093dca1f7917fe776666df8b4643
MrAntoine <antoinevdb15@gmail.com>
2019-02-05 16:50:06 +0100

    Ajout enimation like unlike

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,47 +4,33 @@ $( document ).ready(function() {
 });
 
 
-
+//LIKES
 $(function() {
-//$(document).ready(function(){
 
-    $('.likes').click(function() {
-        var color = $( this ).css( "background-color" );
-        let mavar = $( this );
+    $('form.likesForm').submit(function(event){
+        //return false;
+        event.preventDefault();
+
+        var color = $( this ).find('.likes').css( "background-color" );
+        let mavar = $( this ).find('.likes');
         //console.log($(this ).parent().serializeArray());
 
         //$.post('vues/likes.php',
-        $.post( $("form").attr("action"),
-            $(this ).parent().serializeArray(),
+        $.post( $(this).attr("action"),
+            $(this ).serializeArray(),
             function(data) {
-               /*if( data == 'vrai'){
-                    //console.log(data);
-                    $(mavar).css( "background-color", 'black' );
-                }else {
-                    //console.log(data);
-                    $(mavar).css( "background-color", 'red' );
-                }*/
+                if( data == 'like'){
+                     $(mavar).css( "background-color", 'black' );
+                 }else {
+                     $(mavar).css( "background-color", 'red' );
+                 }
 
 
-              //document.write(data);
-               // console.log($("div.col-md-12.main").val());
-                //console.log(data);
+
             });
 
-        if(color == "rgb(255, 0, 0)"){
-            $(mavar).css( "background-color", 'black' );
-        }else {
-            $(mavar).css( "background-color", 'red' );
-        }
-
-
     });
 
-
-    $('form').submit(function(event){
-        //return false;
-        event.preventDefault();
-    });
 
 
 });
