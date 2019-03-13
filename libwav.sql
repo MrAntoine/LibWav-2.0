e055deff85c031c0e2a033cc66b199bda240eb10
MrAntoine <antoinevdb15@gmail.com>
2019-03-13 21:37:07 +0100

    MAJ DB (13 mars)

--- libwav.sql
+++ libwav.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 08 mars 2019 à 19:02
+-- Généré le :  mer. 13 mars 2019 à 20:36
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -50,72 +50,18 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
   `date_publi` date NOT NULL,
   `id_Post` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `commentaires`
 --
 
 INSERT INTO `commentaires` (`id`, `contenu`, `idCreateur`, `nb_like`, `date_publi`, `id_Post`) VALUES
-(1, 'Mon commentaire', 1, 0, '2008-03-20', 0),
-(2, 'COUCOU', 1, 0, '2008-03-20', 1),
-(3, 'COUCOU', 1, 0, '2008-03-20', 1),
-(4, 'COUCOU', 1, 0, '2008-03-20', 1),
-(5, 'COUCOU', 1, 0, '2008-03-20', 1),
-(6, 'COUCOU', 1, 0, '2008-03-20', 1),
-(7, 'TOTO', 1, 0, '2008-03-20', 3),
-(8, 'TEST', 1, 0, '2008-03-20', 2),
-(9, 'TEST', 1, 0, '2008-03-20', 2),
-(10, 'entre ton commetaire mdr', 1, 0, '2008-03-20', 5),
-(11, 'entre ton commetaire mdr', 1, 0, '2008-03-20', 5),
-(12, 'entre ton commetaire mdr', 1, 0, '2008-03-20', 5),
-(13, 'entre ton commetaire mdr', 1, 0, '2008-03-20', 5),
-(14, 'entre ton commetaire mdr', 1, 0, '2008-03-20', 5),
-(15, 'ok lol', 1, 0, '2008-03-20', 5),
-(16, 'ok lol', 1, 0, '2008-03-20', 5),
-(17, 'ok lol', 1, 0, '2008-03-20', 5),
-(18, 'ok lol', 1, 0, '2008-03-20', 5),
-(19, 'ok lol', 1, 0, '2008-03-20', 5),
-(20, 'ok lol', 1, 0, '2008-03-20', 5),
-(21, 'mdr', 1, 0, '2008-03-20', 5),
-(22, 'lol', 1, 0, '2008-03-20', 5),
-(23, 'lol', 1, 0, '2008-03-20', 5),
-(24, 'lol', 1, 0, '2008-03-20', 5),
-(25, 'lol', 1, 0, '2008-03-20', 5),
-(26, 'ok', 1, 0, '2008-03-20', 5),
-(27, 'trololo', 1, 0, '2008-03-20', 5),
-(28, 'XD', 1, 0, '2008-03-20', 5),
-(29, 'fzfok', 1, 0, '2008-03-20', 5),
-(30, 'ml;lm', 1, 0, '2008-03-20', 5),
-(31, 'joijoijoi', 1, 0, '2008-03-20', 5),
-(32, 'kopkop', 1, 0, '2008-03-20', 5),
-(33, 'ANTOINE', 1, 0, '2008-03-20', 5),
-(34, 'LE WEB', 1, 0, '2008-03-20', 5),
-(35, 'mYSQL', 1, 0, '2008-03-20', 5),
-(36, 'CEci est un test <br/> Avec un retrour  a la <b>ligne</b>', 1, 0, '2008-03-20', 5),
-(37, '<script> alert(\"coucou\");</script>', 1, 0, '2008-03-20', 5),
-(38, 'CC', 1, 0, '2008-03-20', 6),
-(39, 'TETEE', 1, 0, '2008-03-20', 6),
-(40, 'qetrg', 1, 0, '2008-03-20', 6),
-(41, 'AZERTY', 1, 0, '2008-03-20', 6),
-(42, 'FRIDAY', 1, 0, '2008-03-20', 6),
-(43, 'ARMA III', 1, 0, '2008-03-20', 6),
-(44, 'ARMA IV', 1, 0, '2008-03-20', 6),
-(45, 'ARMA V', 1, 0, '2008-03-20', 6),
-(46, 'VENDREDI', 1, 0, '2008-03-20', 6),
-(47, 'SAMEDI', 1, 0, '2008-03-20', 6),
-(48, 'DIMANCHE', 1, 0, '2008-03-20', 6),
-(49, '^pmlp^lmp^l', 1, 0, '2008-03-20', 6),
-(50, 'trololo', 1, 0, '2008-03-20', 6),
-(51, 'okpokpok', 1, 0, '2008-03-20', 6),
-(52, 'popopo', 1, 0, '2008-03-20', 6),
-(53, 'lp^lp^l^pl', 1, 0, '2008-03-20', 6),
-(54, 'AMAZING', 1, 0, '2019-03-20', 6),
-(55, 'sdf', 1, 0, '2008-03-20', 6),
-(56, 'TANEINO', 1, 0, '2008-03-20', 6),
-(57, 'TANEINO II', 1, 0, '2008-03-20', 6),
-(58, 'BIKINI', 1, 0, '2008-03-20', 6),
-(59, 'ON MANGE', 1, 0, '2008-03-20', 6);
+(1, 'Voici un commentaire', 1, 0, '2008-03-20', 6),
+(2, 'Voici un commentaire', 1, 0, '2007-03-20', 6),
+(3, 'Voici un commentaire', 1, 0, '2008-03-21', 6),
+(4, 'Voici un commentaire', 1, 0, '2008-03-19', 6),
+(5, 'tyer', 1, 0, '2012-03-20', 6);
 
 -- --------------------------------------------------------
 
@@ -168,7 +114,14 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `id_user` int(11) NOT NULL,
   `id_contenu` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=768 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=790 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `likes`
+--
+
+INSERT INTO `likes` (`id`, `id_user`, `id_contenu`) VALUES
+(788, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -290,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `son` (
   `id_son_categorie` int(11) NOT NULL,
   `nb_telechargements` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `son`
@@ -300,7 +253,8 @@ INSERT INTO `son` (`id`, `lien_upload`, `titre`, `description`, `idCreateur`, `d
 (1, '429.mp3', 'Mon premier son ', 'voici la description ', 1, '2019-01-01', 1, 10),
 (2, '429.mp3', 'Toto', '', 1, '2020-01-20', 15, 12),
 (36, 'Un Gars Une Fille en 4K - Reproduction du Générique - Jingle Un Gars Une Fille - UHD - Fictif - 2016.mp3', 'mec', '', 1, '2009-02-20', 0, 0),
-(35, '7803.wav', 'Toto2', '', 1, '2009-02-20', 0, 0);
+(35, '7803.wav', 'Toto2', '', 1, '2009-02-20', 0, 0),
+(37, '429.mp3', 'Toto35434', '', 1, '2020-01-20', 15, 12);
 
 -- --------------------------------------------------------
 
@@ -313,7 +267,7 @@ CREATE TABLE IF NOT EXISTS `son_categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `categorie_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `son_categorie`
@@ -354,7 +308,7 @@ CREATE TABLE IF NOT EXISTS `tutos` (
   `nb_like` int(11) NOT NULL,
   `id_article_categorie` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `tutos`
@@ -363,6 +317,7 @@ CREATE TABLE IF NOT EXISTS `tutos` (
 INSERT INTO `tutos` (`id`, `titre`, `description`, `video_lien`, `contenu`, `idCreateur`, `date_publi`, `nb_like`, `id_article_categorie`) VALUES
 (5, 'Test embed', 'Test embedTest embed', 'https://www.youtube.com/watch?v=ugkK6_XmsaQ', 'Test embedTest embed', 1, '2008-03-20', 0, NULL),
 (6, 'Test embed', 'Test embedTest embed', 'https://www.youtube.com/embed/ugkK6_XmsaQ', 'Test embedTest embed', 1, '2008-03-20', 0, NULL),
+(7, 'LUNDI', '<script>alert(\"coucou\");</script> ', 'https://www.youtube.com/embed/ugkK6_XmsaQ', 'opkopkpk', 2, '2013-03-20', 0, NULL),
 (2, 'kl,k,lkl,,k,', 'k,lk,,,,,,,,,,,,,', 'https://www.youtube.com/embed/ugkK6_XmsaQ', 'k,lk,klkl,,klllllllllllll', 1, '2007-03-20', 0, NULL),
 (3, 'gyuioyhgbyuhb', 'bjbjhbjhb', 'http://www.allocine.fr/video/', 'hjbhjbjhbjh', 1, '2007-03-20', 0, NULL),
 (4, 'facebook', 'wdgfh vugjlhpol;u,yfnjhbgvfcdss', 'https://www.facebook.com/?ref=tn_tnmn', ':opmpli;,nbvfcdxwqsxzdcfvgbhnj,k;l:m!m:l;ik,jnbvqwszxdcefvrgyhnju,ki;lo:', 1, '2007-03-20', 0, NULL);
@@ -379,7 +334,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `pseudo` varchar(255) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `prenom` varchar(255) DEFAULT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT 'avatar_default.JPG',
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `sexe` varchar(255) DEFAULT NULL,
@@ -392,14 +347,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `nb_telechargements` int(11) NOT NULL DEFAULT '0',
   `points` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id`, `pseudo`, `nom`, `prenom`, `avatar`, `email`, `password`, `sexe`, `anniversaire`, `region`, `pays`, `statut`, `created_at`, `temps_connexion`, `nb_telechargements`, `points`) VALUES
-(1, 'Taneino', 'Vdb', 'Antoine', 'IMG_2862.JPG', 'antoinevdb15@gmail.com', '123456789', 'Homme', '1999-01-15', 'Hauts-De-France', 'France', '5', '2019-01-17', '00:00:03', 20, 840);
+(1, 'Taneino', 'Vdb', 'Antoine', 'IMG_2862.JPG', 'antoinevdb15@gmail.com', '123456789', 'Homme', '1999-01-15', 'Hauts-De-France', 'France', '5', '2019-01-17', '00:00:03', 22, 860),
+(2, 'Antonio', NULL, NULL, NULL, 'azuivgfdfbhgfd@fgmai.com', '*CC67043C7BCFF5EEA5566BD9B1F3C74FD9A5CF5D', NULL, NULL, NULL, NULL, '3', '2019-03-13', '00:00:00', 0, 10);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
