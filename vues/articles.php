a72d431f4bb2d1304fae2fc9b7aee503d8dce28e
traccoenlea <leatraccoen@gmail.com>
2019-03-17 12:28:01 +0100

    page article créée + modif pas css ni mise en page

--- vues/articles.php
+++ vues/articles.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./assets/css/articles.css" type="text/css">
+<link rel="stylesheet" href="../assets/css/articles.css" type="text/css">
 <?php
 
 ?>
@@ -7,8 +7,6 @@
     <h1 class="front">Articles</h1>
     <article>
         <div id="contenu">
-            <img src="https://tse1.mm.bing.net/th?id=OIP.Mb3wWgl7e2XZTvhdx5vC1wHaJJ&pid=15.1&P=0&w=300&h=300" alt="image de l'article" id="imgleft"/>
-            <img src="https://tse2.mm.bing.net/th?id=OIP.WmM_3LTdDqzp8MPSIySnYgHaDn&pid=15.1&P=0&w=308&h=151" alt="seconde image" id="litimg" />
             <div id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae luctus magna, a bibendum magna. Etiam
             feugiat nulla sit amet augue mollis rhoncus. Vivamus tristique viverra sollicitudin. Quisque venenatis pulvinar
             gravida. Sed egestas volutpat porta. Nullam semper hendrerit turpis in dictum. Fusce vel porttitor erat. Etiam
