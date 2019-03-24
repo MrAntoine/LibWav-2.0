807d19b816aae24fdb1892add26f158403f2fc42
traccoenlea <leatraccoen@gmail.com>
2019-03-24 18:43:29 +0100

    FAQ

--- vues/communaute.php
+++ vues/communaute.php
@@ -5,6 +5,43 @@
 <div id="faq">
     <h2>Foire Aux Questions</h2>
 
-    <div class="question">Ici la question</div>
-    <blockquote class="réponse">Ici la réponse</blockquote>
+    <ol>
+        <div class="question"><li>Pourquoi Libwav ?</li></div>
+        <blockquote class="réponse">Après des heures et des heures de réflexion (et nous ne plaisantons pas quand nous
+            disons des heures) nous nous sommes arrêtés sur “LibWav”. LibWav a plusieurs significations. Premièrement
+            c’est une contraction de deux mots, “Libre” et “Wave”. Tu auras donc compris libre pour “libre de droits”,
+            “liberté”, et wave pour la vague, les ondes du son mais aussi le fichier .wav qui est un format de fichier
+            audio.
+        </blockquote>
+
+        <div class="question"><li>C’est quoi Libwav ?</li></div>
+        <blockquote class="réponse">LibWav c’est un site trop cool (ouais on fait notre pub, mais en même temps, c’est
+            notre site, on fait ce qu’on veut -ou presque), sur lequel tu peux télécharger des sons de tous genres,
+            toutes humeurs et toutes longueurs de façon GRATUITE et LÉGALE puisque les fichiers sont libres de droits.
+        </blockquote>
+
+        <div class="question"><li>Pourquoi autant d’informations sont demandées dans le compte ?</li></div>
+        <blockquote class="réponse">Pour la simple et bonne raison qu’on veut savoir qui tu es ! Sérieusement, nous
+            sommes un groupe d’étudiants, comme c’est expliqué <b>ici</b> (clique, aller, tu verras, on est cools) et par
+            conséquent, on voulait savoir les différents types de personnes étant intéressés par le téléchargement
+            GRATUIT et LÉGAL de sons.
+        </blockquote>
+
+        <div class="question"><li>Que faites-vous de mes données ?</li></div>
+        <blockquote class="réponse">Les données que l’on récolte sont uniquement utiles pour nos statistiques, afin de
+            savoir d’où tu viens, qui tu es, jusqu’où notre site est allé, quel type de population il touche, la tranche
+            d’âge moyenne, le sexe, le lieu de vie, enfin toutes les infos que tu as rempli ! (Et si tu ne l’as toujours
+            pas fait, tu peux aller <b>ici</b> et le faire, t’en as pour trois secondes et demie, après tout tu te connais
+            mieux que nous !).
+        </blockquote>
+
+
+        <div class="question"><li>Comment prononce-t-on "Libwav" ?</li></div>
+        <blockquote class="réponse">C'est tout simple, et c'est cadeau : lib wouève (lisez-le à haute voix et
+            rapidement...). Non, ce n'est pas allemand ou créole ! Juste un peu d'anglais, sinon, le premier point vous
+            explique tout !
+        </blockquote>
+
+    </ol>
+
 </div>
