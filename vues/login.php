090cf660c9ca77f5218d8bdcd39f291f2b32d727
traccoenlea <leatraccoen@gmail.com>
2019-03-26 08:53:17 +0100

    CSS inscription

--- vues/login.php
+++ vues/login.php
@@ -15,14 +15,14 @@ echo "<form action=\"index.php?action=connexion\" method=\"POST\">
 echo "<br><br><br>";
 */
 //CREATION COMPTE
-echo "<div class=\"\">
-        <h1>Pas encore inscrit(e) ?</h1><h2>Mais qu'attendez-vous ?</h2><h1>Inscrivez-vous vite ! </h1>";
+echo "<div class=\"register\">
+        <h1>Pas encore inscrit(e) ?</h1><h3>Mais qu'attendez-vous ?</h3><h1>Inscrivez-vous vite ! </h1>";
 echo "<form method=\"post\" action=\"index.php?action=register\" name=\"creation\">
             <input type=\"text\" required name=\"pseudo\" placeholder=\"Votre pseudo\">
             <input type=\"email\" required name=\"email\" placeholder=\"Votre e-mail\">
             <input type=\"password\" required name=\"password\" placeholder=\"Votre mot de passe\">
-            <input type=\"password\" required name=\"passwordconf\" placeholder=\"Confirmez votre mot de passe\">";
-echo "<input type=\"submit\" name=\"send\" value=\"Être enfin inscrit(e) sur LibWav!\">
+            <input type=\"password\" required name=\"passwordconf\" id='password_check' placeholder=\"Confirmez votre mot de passe\">";
+echo "<input type=\"submit\" name=\"send\" value=\"Être enfin inscrit(e) sur LibWav !\">
         </form>
     </div></div>";
 
