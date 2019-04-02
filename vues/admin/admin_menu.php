74c4e2bcf3f6c248f42de806a0cae806bd6a4e53
traccoenlea <leatraccoen@gmail.com>
2019-04-02 15:21:34 +0200

    CSS menu admin

--- vues/admin/admin_menu.php
+++ vues/admin/admin_menu.php
@@ -1,12 +1,6 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Antoine
- * Date: 06/02/2019
- * Time: 12:49
- */
 
-echo "<a id='signalements_btn_admin'>Afficher la liste des signalements</a> <div id='signalements_list'></div>";
-echo "<a id='stats_btn_admin'>Statistiques générales</a> <div id='stats_list'></div>";
-
-?>
+<div class="admin_button">
+<button><a id='signalements_btn_admin'>Afficher la liste des signalements</a></button>
+<div id='signalements_list'></div>
+<button><a id='stats_btn_admin'>Statistiques générales</a> <div id='stats_list'></button></div>
+</div>
