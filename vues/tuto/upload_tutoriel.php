b46f6d20c99312478df85a399011c25f4ce47d6e
traccoenlea <leatraccoen@gmail.com>
2019-04-02 16:27:14 +0200

    CSS tutos done

--- vues/tuto/upload_tutoriel.php
+++ vues/tuto/upload_tutoriel.php
@@ -33,21 +33,20 @@ if ($autorisation === true) {
 
 
 
-
     ?>
-
+    <div id="ajout_tuto">
     <form method="post" enctype="multipart/form-data" action="index.php?action=tutorielUpload">
         <p>
-            <input type='text' name='post_video' placeholder='Entrez le lien de la vidéo' required>
-            <input type='text' name='post_title' placeholder='Entrez un titre' required>
+            <input type='text' name='post_video' id='post_video' placeholder='Entrez le lien de la vidéo' required><br />
+            <input type='text' name='post_title' id='post_title' placeholder='Entrez un titre' required>
             <textarea rows="4" cols="50" name="post_description" maxlength="100" placeholder='Entrez une courte description' required></textarea>
             <textarea rows="4" cols="50" name="post_contenu" placeholder='Entrez le contenu' required></textarea>
-            <input type='checkbox' name='conditions_utilisation' required>J'accepte les conditions générales
-            d'utilisation, et d'upload de fichier sonore
+            <span id="checkbox"><input type='checkbox' name='conditions_utilisation' required>J'accepte les <br><a href="?action=cgu">conditions générales
+                    d'utilisation</a>, et d'upload de fichier sonore</span>
             <input type="submit" name="upload" value="Uploader">
         </p>
     </form>
-
+    </div>
     <?php
 }
 
